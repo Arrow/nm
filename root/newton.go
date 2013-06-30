6141cf53d41cdc2b9eec27f1a912ebcf465fec0f
David Samborski <bloggingarrow@gmail.com>
2013-06-30 00:19:21 -0500

    Update Newton root finding method

--- root/newton.go
+++ root/newton.go
@@ -2,15 +2,31 @@ package root
 
 import (
 	"github.com/Arrow/nm/util"
-	derive "github.com/Arrow/nm/differentiation"
 )
 
-func Newton(f util.Function, x0 float64) (x float64) {
+type Newton struct {
+	Max_Iter int
+	Tol      float64
+	fn       util.FuncDiffer
+}
+
+func NewNewton(f util.FuncDiffer) *Newton {
+	n := new(Newton)
+	n.Max_Iter = max_Iter
+	n.Tol = tol
+	return n
+}
+
+func (n *Newton) Compute(x0 float64) (x float64) {
+	f := n.fn.Function()
+	fp := n.fn.Diff()
 	p0 := x0
-	p  := p0
-	for i := 0; i < Max_Iter; i++ {
-		p = p0 - f(p0) / derive.FivePoint(f, p0)
-		if util.Tolerance(p, p0, tol) { return p }
+	p := p0
+	for i := 0; i < n.Max_Iter; i++ {
+		p = p0 - f(p0)/fp(p0)
+		if util.Tolerance(p, p0, n.Tol) {
+			return p
+		}
 	}
 	p0 = p
 	return p
--- root/params.go
+++ root/params.go
@@ -1,11 +1,11 @@
 package root
 
 var (
-	Max_Iter int
-	tol float64
+	max_Iter int
+	tol      float64
 )
 
 func init() {
-	Max_Iter = 100
+	max_Iter = 100
 	tol = 0.001
 }
